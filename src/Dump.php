4c35fb3d87ee1295f9256dc3e91676b37ac4054e
Gabor Zemko <gaborzemko@gmail.com>
2023-02-08 08:44:12 +0100

    Revert last commit.

--- src/Dump.php
+++ src/Dump.php
@@ -170,11 +170,6 @@ class Dump
         if (!in_array($table, $this->completedAdditionalRowsTables, true)) {
             // néhány további sor, szintetikus adatok
             foreach ($this->listAdditionalRows($table) as $rowAdditional) {
-                $rowAdditional = $this->processRow($table, $rowAdditional);
-                if ($rowAdditional === null) {
-                    continue;
-                }
-
                 $this->addToDump(SQLUtils::buildInsertSQL($table, $rowAdditional) . ";\n");
                 $cnt++;
             }
